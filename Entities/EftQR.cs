972a9846a2a5c7d18f9dd895287d4f4a9e01eb0e
Andres Echevarria <santyxz123@gmail.com>
2026-06-20 22:57:32 -0300

    Agrego constructor a eftQR

--- Entities/EftQR.cs
+++ Entities/EftQR.cs
@@ -5,5 +5,11 @@ public class PagoEftqr: Pago
     private double vuelto;
 
     public double Vuelto {get {return this.vuelto;} set{this.vuelto=value;}}
+    
+
+    public PagoEftqr (double monto, Cliente cliente, int idPago, double recargo, double vuelto):base(monto, cliente, idPago, recargo)
+    {
+        this.vuelto=vuelto;
+    }
 
 }
